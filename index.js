adeea99f3fa6775c4c5507b5fb19daf4361b8e2b
robertay99 <robertay99@hotmail.com>
2024-10-22 20:42:22 -0300

    updated index.js

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((tutorial) => {
+    return tutorial
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  });
 }
+
+const tutorialsCapitalized = titleCased();
+console.log(tutorialsCapitalized);
